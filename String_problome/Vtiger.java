fd883de4be944131370c30d57751fd0c77970cf4
ajmalshakeer <ajmalrozario2k17@gmail.com>
2023-12-22 23:11:17 +0530

    updated code for the problome statement on string formating has been uploaded

--- String_problome/Vtiger.java
+++ String_problome/Vtiger.java
@@ -4,9 +4,9 @@ public class Vtiger {
 
 
     //initializing a method to centralizing the input text
-    public static String centralizeString(String words, int maxLength) {
+    public  String centralizeString(String words, int maxLength) {
         String[] wordArray = words.split("\\s+");
-        StringBuilder formattedText = new StringBuilder();
+        StringBuilder formattedText = new StringBuilder();//using the proper string methods
         StringBuilder line = new StringBuilder();
 
         int wordCount = 0;
@@ -16,7 +16,7 @@ public class Vtiger {
             wordCount++;
 
 
-            //appliying the logic for formatting and allignment the string
+            //Applying the logic for formatting and alignment the string
             //along with using some string functions
             if (wordCount == 10 || line.length() > maxLength) {
                 formattedText.append(centralizeLine(line.toString().trim(), maxLength)).append("\n");
@@ -36,7 +36,7 @@ public class Vtiger {
     //initializing another method to format each line to ensure proper centralization
     private static String centralizeLine(String line, int maxLength) {
 
-        int leftPadding = 33;    //Setting the desired left paddin
+        int leftPadding = 33;    //Setting the desired left padding
 
         StringBuilder formattedLine = new StringBuilder();
 
@@ -51,10 +51,11 @@ public class Vtiger {
 
     public static void main(String[] args) {
         String words = "Vtiger CRM is a comprehensive customer relationship management(CRM) software designed to streamline and enhance business operations.Vtiger CRM provides a range of features that cater to various aspects of customer relationship management,including sales automation,marketing automation,customer support& invetory management.";
-
         int maxLength = 40;
-
-        String formattedString = centralizeString(words, maxLength);
-        System.out.println(formattedString);
+        
+        Vtiger obj = new Vtiger();
+        
+        String FormattedString = obj.centralizeString(words, maxLength);
+        System.out.println(FormattedString);
     }
 }
