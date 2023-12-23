36580586e8ed68855b5bf5b41d5029d8cb3e1937
ajmalshakeer <ajmalrozario2k17@gmail.com>
2023-12-23 14:01:25 +0530

    updated code commited

--- String_problome/Vtigermain.java
+++ String_problome/Vtigermain.java
@@ -1,11 +1,6 @@
 package String_problome;
 
 public class Vtigermain {
-    
-
-
-   
-
     public static void main(String[] args) {
         String words = "Vtiger CRM is a comprehensive customer relationship management(CRM) software designed to streamline and enhance business operations.Vtiger CRM provides a range of features that cater to various aspects of customer relationship management,including sales automation,marketing automation,customer support& invetory management.";
         int maxLength = 40;
