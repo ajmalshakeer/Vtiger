b3484597ee5259762fbaca8c395f363b7b8a6d95
ajmalshakeer <ajmalrozario2k17@gmail.com>
2023-12-25 20:50:32 +0530

    updated code

--- String_problome/Vtigermain.java
+++ String_problome/Vtigermain.java
@@ -2,7 +2,7 @@ package String_problome;
 
 public class Vtigermain {
     public static void main(String[] args) {
-        String words = "Vtiger CRM is a comprehensive customer relationship management(CRM) software designed to streamline and enhance business operations.";
+        String words = "Vtiger CRM is a comprehensive customer relationship management(CRM) software designed to business operations.";
         int maxLength = 40;
         
         Vtiger_function obj = new Vtiger_function();
